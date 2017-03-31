a9efd1054ca0c2732b44d1917bbbc3f60215296b
Sander De la Marche <sanderdlm69@gmail.com>
2017-03-31 10:52:27 +0200

    Added some comments to the home page template (for clarification)

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,49 +1,60 @@
 <?php snippet('header') ?>
 
-  <main class="main" role="main">
+<main class="main" role="main">
 
-    <?php snippet('menu') ?>
-    
-    <section id="intro">
-      <div class="weekly">
-        <p>what m+ garbage do i have to deal with this week?</p>
-        <div class='weekly_us'></div>
-        <div class='weekly_eu'></div>
-      </div>
-      <div class="text">
-        <?= $page->text()->kirbytext() ?>
-      </div>
+  <?php snippet('menu') ?>
+
+  <!-- The "intro" section - above the fold - Weekly affix display -->
+  <section id="intro">
+
+    <div class="weekly">
+      <p>what m+ garbage do i have to deal with this week?</p>
+      <div class='weekly_us'></div>
+      <div class='weekly_eu'></div>
+    </div>
+
+    <div class="text">
+      <!-- Here we output the "home" text field, can be used for updates, notices, etc.. -->
+      <?= $page->text()->kirbytext() ?>
+    </div>
+
+  </section>
+  
+  <!-- The "next" section - right under the fold - Next & week after affix display -->
+  <section id="next">
+
+    <div class="nextweek">
+      <p>Next week</p>
+      <div class='nextweekus'></div>
+      <div class='nextweekeu'></div>
+    </div>
+
+    <div class="weekafter">
+      <p>Week after next</p>
+      <div class='weekafternextus'></div>
+      <div class='weekafternexteu'></div>
+    </div>
+
+  </section>
+  
+  <!-- Loop over all the pages in the back-end, and create a section for each of them -->
+  <!-- This way we get a one-page website, but with different manageable pages in the CMS -->
+  <?php foreach($pages->visible() as $section): ?>
+
+    <section id="<?= $section->uid() ?>">
+      <h2><?= $section->title()->html() ?></h2>
+      <div class="text"><?= $section->text()->kirbytext() ?></div>
+
+      <?php if($section->affixes()->yaml() != null): // If the page has an affixis field, load the affixes snippet ?>
+
+        <?php snippet($section->uid(), array('data' => $section)); ?>
+
+      <?php endif ?>
+
     </section>
 
-    <section id="next">
-      <div class="nextweek">
-        <p>Next week</p>
-        <div class='nextweekus'></div>
-        <div class='nextweekeu'></div>
-      </div>
-      <div class="weekafter">
-        <p>Week after next</p>
-        <div class='weekafternextus'></div>
-        <div class='weekafternexteu'></div>
-      </div>
-    </section>
-    
-    <?php foreach($pages->visible() as $section): ?>
-
-      <section id="<?= $section->uid() ?>">
-        <h2><?= $section->title()->html() ?></h2>
-        <div class="text"><?= $section->text()->kirbytext() ?></div>
-
-        <?php if($section->affixes()->yaml() != null): // If the page has an affixis field, load the affixes snippet ?>
-
-            <?php snippet($section->uid(), array('data' => $section)); ?>
-
-        <?php endif ?>
-        
-      </section>
-
-    <?php endforeach ?>
+  <?php endforeach ?>
   
   <?php snippet('footer') ?>
 
-  </main>
+</main>
