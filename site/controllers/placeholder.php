8859e493b2baf780a2290fff2d38fcdccd2c8db4
Sander De la Marche <sanderdlm69@gmail.com>
2017-03-31 10:53:12 +0200

    Renamed the blog controller to an empty placeholder, maybe used later on

--- site/controllers/placeholder.php
+++ site/controllers/placeholder.php
@@ -9,15 +9,5 @@
 
 return function($site, $pages, $page) {
 
-  $perpage  = $page->perpage()->int();
-  $articles = $page->children()
-                   ->visible()
-                   ->flip()
-                   ->paginate(($perpage >= 1)? $perpage : 5);
-
-  return [
-    'articles'   => $articles,
-    'pagination' => $articles->pagination()
-  ];
-
+  
 };
