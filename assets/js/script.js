0caa60cee4db62790fb2279c839297c8a3f57be6
Chet <sanderdlm69@gmail.com>
2017-04-08 21:01:53 +0200

    Updated script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,16 @@
  /**
-  * calc_weeks  - returns the current, next and third week coming up in the rotation based on the current date
-  * @param Date -  reference_date
-  * @return Integer - week number between 1 and 8
+  * calc_weeks  - returns the first, second and third_week week coming up in the rotation based on the current date
+  * @param Date -  reference_date (fresh start of the rotation)
+  * @return Integer - week number between 0 and 7
   */ 
 function calc_weeks(reference_date){
 	var d = new Date();
-	var diffDays = Math.round(Math.abs((reference_date.getTime() - d.getTime())/(24*60*60*1000)));
+	var diffDays = Math.round((d.getTime() - reference_date.getTime())/(24*60*60*1000));
 	var diffWeeks = Math.floor(diffDays/7);
 	var w = (diffWeeks%8);
-	var weeks = [w, w+1, w+2];
+	var weeks = {"first_week":w, "second_week":w+1, "third_week":w+2};
 	for(index in weeks){
-		if(weeks[index] >= 9){
-			weeks[index] = weeks[index] - 8;
-		}
+		(weeks[index] >= 8) ? weeks[index] = weeks[index] - 8 : null;
 	}
 	return weeks;
 }
@@ -21,51 +19,50 @@ function calc_weeks(reference_date){
  *		Variables
  */
 
-var reference_date_eu = new Date(Date.UTC(2017, 2, 29, 7, 0, 0));
-var reference_date_us = new Date(Date.UTC(2017, 2, 28, 15, 0, 0));
+var reference_date_eu = new Date(Date.UTC(2017, 2, 29, 7, 0, 0)); //Patch 7.2 launch date
+var reference_date_us = new Date(Date.UTC(2017, 2, 28, 15, 0, 0)); //Patch 7.2 launch date
 var schedule = [
-	[{ "name": "raging", "difficulty": 1 },{ "name": "volcanic", "difficulty": 0 },{ "name": "tyrannical", "difficulty": 2 }],
-	[{ "name": "?", "difficulty": 0 },{ "name": "?", "difficulty": 0 },{ "name": "?", "difficulty": 2 }],
-	[{ "name": "?", "difficulty": 0 },{ "name": "?", "difficulty": 0 },{ "name": "?", "difficulty": 2 }],
-	[{ "name": "?", "difficulty": 0 },{ "name": "?", "difficulty": 0 },{ "name": "?", "difficulty": 2 }],
-	[{ "name": "?", "difficulty": 0 },{ "name": "?", "difficulty": 0 },{ "name": "?", "difficulty": 2 }],
-	[{ "name": "?", "difficulty": 0 },{ "name": "?", "difficulty": 0 },{ "name": "?", "difficulty": 2 }],
-	[{ "name": "?", "difficulty": 0 },{ "name": "?", "difficulty": 0 },{ "name": "?", "difficulty": 2 }],
-	[{ "name": "?", "difficulty": 0 },{ "name": "?", "difficulty": 0 },{ "name": "?", "difficulty": 2 }],
+	{ "first_affix": { "name": "raging", "difficulty": "med" }, "second_affix": { "name": "volcanic", "difficulty": "easy" }, "third_affix": { "name": "tyrannical", "difficulty": "hard" }},
+	{ "first_affix": { "name": "teeming", "difficulty": "med" }, "second_affix": { "name": "explosive", "difficulty": "easy" }, "third_affix": { "name": "fortified", "difficulty": "hard" }},
+	{ "first_affix": { "name": "?", "difficulty": "easy" }, "second_affix": { "name": "?", "difficulty": "easy" }, "third_affix": { "name": "?", "difficulty": "hard" }},
+	{ "first_affix": { "name": "?", "difficulty": "easy" }, "second_affix": { "name": "?", "difficulty": "easy" }, "third_affix": { "name": "?", "difficulty": "hard" }},
+	{ "first_affix": { "name": "?", "difficulty": "easy" }, "second_affix": { "name": "?", "difficulty": "easy" }, "third_affix": { "name": "?", "difficulty": "hard" }},
+	{ "first_affix": { "name": "?", "difficulty": "easy" }, "second_affix": { "name": "?", "difficulty": "easy" }, "third_affix": { "name": "?", "difficulty": "hard" }},
+	{ "first_affix": { "name": "?", "difficulty": "easy" }, "second_affix": { "name": "?", "difficulty": "easy" }, "third_affix": { "name": "?", "difficulty": "hard" }},
+	{ "first_affix": { "name": "?", "difficulty": "easy" }, "second_affix": { "name": "?", "difficulty": "easy" }, "third_affix": { "name": "?", "difficulty": "hard" }}
 ];
-var difficulties = ["easy", "med", "hard"]; //Difficulty in the schedule is on a 0, 1, 2 scale instead of 1,2,3 for easier array access later
 
 /*
  *		Runtime
  */
 
-var weeks_eu = calc_weeks(reference_date_eu);
-var weeks_us = calc_weeks(reference_date_us);
+var eu = calc_weeks(reference_date_eu);
+var us = calc_weeks(reference_date_us);
 
 //EU
 
-document.getElementsByClassName("weekly_eu")[0].innerHTML = "EU | <span class='"+difficulties[schedule[weeks_eu[0]][0].difficulty]+"'>" + schedule[weeks_eu[0]][0].name + "</span>, "+
-															"<span class='"+difficulties[schedule[weeks_eu[0]][1].difficulty]+"'>" + schedule[weeks_eu[0]][1].name + "</span>, "+
-															"<span class='"+difficulties[schedule[weeks_eu[0]][2].difficulty]+"'>" + schedule[weeks_eu[0]][2].name + "</span>";
+document.getElementsByClassName("weekly_eu")[0].innerHTML = "EU | <span class='"+schedule[eu.first_week].first_affix.difficulty+"'>" + schedule[eu.first_week].first_affix.name + "</span>, "+
+															"<span class='"+schedule[eu.first_week].second_affix.difficulty+"'>" + schedule[eu.first_week].second_affix.name + "</span>, "+
+															"<span class='"+schedule[eu.first_week].third_affix.difficulty+"'>" + schedule[eu.first_week].third_affix.name + "</span>";
 
-document.getElementsByClassName("nextweekeu")[0].innerHTML = "EU | <span class='"+difficulties[schedule[weeks_eu[1]][0].difficulty]+"'>" + schedule[weeks_eu[1]][0].name + "</span>, "+
-															"<span class='"+difficulties[schedule[weeks_eu[1]][1].difficulty]+"'>" + schedule[weeks_eu[1]][1].name + "</span>, "+
-															"<span class='"+difficulties[schedule[weeks_eu[1]][2].difficulty]+"'>" + schedule[weeks_eu[1]][2].name + "</span>";
+document.getElementsByClassName("nextweekeu")[0].innerHTML = "EU | <span class='"+schedule[eu.second_week].first_affix.difficulty+"'>" + schedule[eu.second_week].first_affix.name + "</span>, "+
+															"<span class='"+schedule[eu.second_week].second_affix.difficulty+"'>" + schedule[eu.second_week].second_affix.name + "</span>, "+
+															"<span class='"+schedule[eu.second_week].third_affix.difficulty+"'>" + schedule[eu.second_week].third_affix.name + "</span>";
 
-document.getElementsByClassName("weekafternexteu")[0].innerHTML = "EU | <span class='"+difficulties[schedule[weeks_eu[2]][0].difficulty]+"'>" + schedule[weeks_eu[2]][0].name + "</span>, "+
-															"<span class='"+difficulties[schedule[weeks_eu[2]][1].difficulty]+"'>" + schedule[weeks_eu[2]][1].name + "</span>, "+
-															"<span class='"+difficulties[schedule[weeks_eu[2]][2].difficulty]+"'>" + schedule[weeks_eu[2]][2].name + "</span>";
+document.getElementsByClassName("weekafternexteu")[0].innerHTML = "EU | <span class='"+schedule[eu.third_week].first_affix.difficulty+"'>" + schedule[eu.third_week].first_affix.name + "</span>, "+
+															"<span class='"+schedule[eu.third_week].second_affix.difficulty+"'>" + schedule[eu.third_week].second_affix.name + "</span>, "+
+															"<span class='"+schedule[eu.third_week].third_affix.difficulty+"'>" + schedule[eu.third_week].third_affix.name + "</span>";
 
 //US
 
-document.getElementsByClassName("weekly_us")[0].innerHTML = "US | <span class='"+difficulties[schedule[weeks_us[0]][0].difficulty]+"'>" + schedule[weeks_us[0]][0].name + "</span>, "+
-															"<span class='"+difficulties[schedule[weeks_us[0]][1].difficulty]+"'>" + schedule[weeks_us[0]][1].name + "</span>, "+
-															"<span class='"+difficulties[schedule[weeks_us[0]][2].difficulty]+"'>" + schedule[weeks_us[0]][2].name + "</span>";
+document.getElementsByClassName("weekly_us")[0].innerHTML = "US | <span class='"+schedule[us.first_week].first_affix.difficulty+"'>" + schedule[us.first_week].first_affix.name + "</span>, "+
+															"<span class='"+schedule[us.first_week].second_affix.difficulty+"'>" + schedule[us.first_week].second_affix.name + "</span>, "+
+															"<span class='"+schedule[us.first_week].third_affix.difficulty+"'>" + schedule[us.first_week].third_affix.name + "</span>";
 
-document.getElementsByClassName("nextweekus")[0].innerHTML = "US | <span class='"+difficulties[schedule[weeks_us[1]][0].difficulty]+"'>" + schedule[weeks_us[1]][0].name + "</span>, "+
-															"<span class='"+difficulties[schedule[weeks_us[1]][1].difficulty]+"'>" + schedule[weeks_us[1]][1].name + "</span>, "+
-															"<span class='"+difficulties[schedule[weeks_us[1]][2].difficulty]+"'>" + schedule[weeks_us[1]][2].name + "</span>";
+document.getElementsByClassName("nextweekus")[0].innerHTML = "US | <span class='"+schedule[us.second_week].first_affix.difficulty+"'>" + schedule[us.second_week].first_affix.name + "</span>, "+
+															"<span class='"+schedule[us.second_week].second_affix.difficulty+"'>" + schedule[us.second_week].second_affix.name + "</span>, "+
+															"<span class='"+schedule[us.second_week].third_affix.difficulty+"'>" + schedule[us.second_week].third_affix.name + "</span>";
 
-document.getElementsByClassName("weekafternextus")[0].innerHTML = "US | <span class='"+difficulties[schedule[weeks_us[2]][0].difficulty]+"'>" + schedule[weeks_us[2]][0].name + "</span>, "+
-															"<span class='"+difficulties[schedule[weeks_us[2]][1].difficulty]+"'>" + schedule[weeks_us[2]][1].name + "</span>, "+
-															"<span class='"+difficulties[schedule[weeks_us[2]][2].difficulty]+"'>" + schedule[weeks_us[2]][2].name + "</span>";
+document.getElementsByClassName("weekafternextus")[0].innerHTML = "US | <span class='"+schedule[us.third_week].first_affix.difficulty+"'>" + schedule[us.third_week].first_affix.name + "</span>, "+
+															"<span class='"+schedule[us.third_week].second_affix.difficulty+"'>" + schedule[us.third_week].second_affix.name + "</span>, "+
+															"<span class='"+schedule[us.third_week].third_affix.difficulty+"'>" + schedule[us.third_week].third_affix.name + "</span>";
